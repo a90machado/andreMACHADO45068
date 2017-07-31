268e0464c4decf8450701980c7a2da19d432f642
Andre Machado <andre.machado@my.istec.pt>
2017-07-31 02:32:15 +0100

    Initial Commit

---
+++ andreMACHADO45068/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  andreMACHADO45068
+//
+//  Created by Andre Machado on 31/07/2017.
+//  Copyright © 2017 Andre Machado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
